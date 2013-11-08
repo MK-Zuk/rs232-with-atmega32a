4c7f825ae7674a37acd22a700cc6fe219d5ec9f5
Marek.kciuk.80 <Marek.kciuk.80@gmail.com>
2013-11-08 20:44:43 +0000

    testowa aplikacja dla RS232

---
+++ RS232 z przerwaniami/trunk/main/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ * Created: 2013-11-08 21:39:54
+ *  Author: Marek
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
