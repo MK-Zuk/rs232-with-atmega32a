8edc60b1454bec4df5a0d075083ef46c817fb07f
Marek.kciuk.80 <Marek.kciuk.80@gmail.com>
2013-11-09 22:15:52 +0000

    dziala nadawanie USART na przerwaniu

--- RS232 z przerwaniami/trunk/main/usart_lib.h
+++ RS232 z przerwaniami/trunk/main/usart_lib.h
@@ -15,9 +15,16 @@
 #define BAUD_8MHZ_19200 25	//err 0,2
 #define BAUD_8MHZ_38400 12	//err 0,2
 
+#define BUFFER_SIZE	6
+#define SEND		0
+#define RECEIVE		1
+
+volatile uint8_t USART_flag;
 
 void USART_init(uint16_t baud);
+void USART_initInt(uint16_t baud);
 void USART_softsend(uint8_t data);
+void USART_sendInt(uint8_t *buff);
 uint8_t USART_softread(void);
 
 void USART_WriteHex(uint8_t num);
